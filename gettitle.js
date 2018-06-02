46cdef3d35b024f321c6d042bc66288b0bd44b7f
Chardonnay <konomusicbox6@gmail.com>
2018-06-02 18:26:32 +0900

    casper getTitle.js ___option --verbose --log-level=debug

--- gettitle.js
+++ gettitle.js
@@ -1,10 +1,16 @@
-//
+//Webサイトからタイトルを表示するプログラム
+
+//対象のurlの取得
 var TARGET_URL = "http://kujirahand.com";
 
+//casperjsのオブジェクトを作製
 var casper = require('casper').create();
 
+//指定のWebサイトを開く
 casper.start(TARGET_URL,function(){
+	//タイトルを表示する
 	this.echo(casper.getTitle());
 });
 
+//処理を実行する
 casper.run();
